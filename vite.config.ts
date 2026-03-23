9a435354d9def7ece1589b756f59e3e5743b5d64
luiztux <luiztux@gmail.com>
2026-03-23 19:08:18 -0300

    ci: Configure GitHub Pages deployment by adding a workflow, setting Vite base, and ignoring build outputs.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/stitch-to-tailwind/", // Nome exato do seu repositório no GitHub
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
