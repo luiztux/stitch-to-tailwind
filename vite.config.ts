9d868c6e7f4bae0a08b61f1b90af0704b036dbb9
luiztux <luiztux@gmail.com>
2026-03-24 09:09:33 -0300

    fix: package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/stitch-to-tailwind/",
+  base: "/stitch-to-tailwind",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
